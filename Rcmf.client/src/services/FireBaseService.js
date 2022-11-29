ec9ef8aad20dd3cabe2cc4f6c59d0b15ec6c5052
daniel-le97 <daniel97.le>
2022-11-29 12:19:30 -0700

    commit

--- Rcmf.client/src/services/FireBaseService.js
+++ Rcmf.client/src/services/FireBaseService.js
@@ -1,18 +1,20 @@
 // import { computed, ref } from "vue";
 import { initializeApp } from "firebase/app";
-import {
-  collection,
-  doc,
-  getFirestore,
-  limit,
-  orderBy,
-  query,
-  addDoc,
-  
-  
-} from "firebase/firestore";
+// import {
+//   collection,
+//   doc,
+//   getFirestore,
+//   limit,
+//   orderBy,
+//   query,
+//   addDoc,
+// } from "firebase/firestore";
 // import firebase from "firebase"
-import { useFirestore} from "@vueuse/firebase/useFirestore";
+
+// import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getDatabase } from "firebase/database";
+// ... other firebase imports
 
 const firebaseConfig = {
   apiKey: "AIzaSyBB4Cc2RvmYrv6lFIzyK3qUnM7RrhT57Q8",
@@ -23,28 +25,29 @@ const firebaseConfig = {
   appId: "1:343563700800:web:f13a16fb76e837a09e2da5",
   measurementId: "G-63LF04F29S",
 };
+export const firebaseApp = initializeApp(firebaseConfig);
+export const database = getDatabase(firebaseApp);
+export const firestore = getFirestore(firebaseApp);
+// ... other firebase exports
+// import { useFirestore } from "@vueuse/firebase/useFirestore";
 
+//  export const db = getFirestore(app);
 
-const app = initializeApp(firebaseConfig);
- export const db = getFirestore(app);
-
-export const chats = useFirestore(collection(db,"chats"));
+// export const chats = useFirestore(collection(db,"chats"));
 
 // const chat = db.collection("chats")
 // const chatsRef = FirebaseDocRef(collection(db,"chats"))
 
 class FiresService {
-  async addChat(chatsData){
+  async addChat(chatsData) {
+    // console.log(app);
+    // console.log(db);
+    // console.log(chats);
 
-  
-
-    const res = await chats.
-    console.log(res);
-
+    console.log(firebaseApp);
+    console.log(firestore);
+    console.log(database);
   }
-  // 
+  //
 }
-export const firesService = new FiresService()
-
-
-
+export const firesService = new FiresService();
