880cf4498fd658497dc937be77c05906b5270b82
daniel-le97 <daniel97.le>
2022-11-29 12:30:50 -0700

    commit

--- Rcmf.client/src/services/FireBaseService.js
+++ Rcmf.client/src/services/FireBaseService.js
@@ -1,19 +1,17 @@
-// import { computed, ref } from "vue";
 import { initializeApp } from "firebase/app";
-// import {
-//   collection,
-//   doc,
-//   getFirestore,
-//   limit,
-//   orderBy,
-//   query,
-//   addDoc,
-// } from "firebase/firestore";
+import {
+  collection,
+  doc,
+  getFirestore,
+  limit,
+  orderBy,
+  query,
+  addDoc,
+} from "firebase/firestore";
 // import firebase from "firebase"
 
-// import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import { getDatabase } from "firebase/database";
+// import { getFirestore } from "firebase/firestore";
+// import { getDatabase } from "firebase/database";
 // ... other firebase imports
 
 const firebaseConfig = {
@@ -26,14 +24,14 @@ const firebaseConfig = {
   measurementId: "G-63LF04F29S",
 };
 export const firebaseApp = initializeApp(firebaseConfig);
-export const database = getDatabase(firebaseApp);
+// export const database = getDatabase(firebaseApp);
 export const firestore = getFirestore(firebaseApp);
 // ... other firebase exports
-// import { useFirestore } from "@vueuse/firebase/useFirestore";
+import { useFirestore } from "@vueuse/firebase/useFirestore";
 
 //  export const db = getFirestore(app);
 
-// export const chats = useFirestore(collection(db,"chats"));
+export const chats = useFirestore(collection(firestore, "chats"));
 
 // const chat = db.collection("chats")
 // const chatsRef = FirebaseDocRef(collection(db,"chats"))
@@ -42,11 +40,11 @@ class FiresService {
   async addChat(chatsData) {
     // console.log(app);
     // console.log(db);
-    // console.log(chats);
 
     console.log(firebaseApp);
+    // console.log(database);
     console.log(firestore);
-    console.log(database);
+    console.log(chats);
   }
   //
 }
