86b5ba6c56e1361c975ad92d53b5d7d90204bd4c
TungLe0319 <tung.le0319@gmail.com>
2022-11-28 22:11:34 -0700

    Puppetier Test - tung

--- Rcmf.client/testPuppet.js
+++ Rcmf.client/testPuppet.js
@@ -12,7 +12,9 @@ const puppeteer = require("puppeteer");
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto("https://github.com/TriLe1122");
-  await page.screenshot({ path: "./assets/img/puppetier/example.png" });
+ await page.select('body > div.application-main > main > div.container-xl.px-3.px-md-4.px-lg-5 > div > div.Layout-sidebar > div > div.js-profile-editable-replace > div.d-flex.flex-column > div.js-profile-editable-area.d-flex.flex-column.d-md-block > div:nth-child(2) > button')
+  await page.screenshot({ path: "src/assets/img/puppetier/test.png" });
 
   await browser.close();
 })();
+//156.36 x 48
