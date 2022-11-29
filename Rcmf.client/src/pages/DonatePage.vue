38d76bcd604385689b64946b2ae224104937e4bf
daniel-le97 <daniel97.le>
2022-11-28 23:42:54 -0700

    commit

--- Rcmf.client/src/pages/DonatePage.vue
+++ Rcmf.client/src/pages/DonatePage.vue
@@ -1,38 +1,47 @@
 <template>
-<div class="container">
-  <div class="row">
-    <div class="col-md-12">
-      DONATE PAGE
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">DONATE PAGE</div>
+      <div>
+        <form @submit.prevent="handleDonate()">
+          <textarea type="text" src="" alt="" v-model="editable" rows="4" />
+          <button type="submit">donate</button>
+        </form>
+      </div>
     </div>
   </div>
-</div>
 </template>
 
 <script>
 import { computed } from "@vue/reactivity";
 import { onMounted, ref, watchEffect } from "vue";
 import { AppState } from "../AppState.js";
-import { logger } from "../utils/Logger.js"
+import { firesService } from "../services/FireBaseService.js";
+import { logger } from "../utils/Logger.js";
 import Pop from "../utils/Pop.js";
 
 export default {
-props:{
-
-       },
+  props: {},
   setup(props) {
-    const editable = ref({});
-    
-    onMounted(() => {
+    const editable = ref("");
 
-    });
+    onMounted(() => {});
     watchEffect(() => {});
 
     return {
       editable,
-      }
-    }
-  }
+      async handleDonate() {
+        try {
+          console.log(editable.value);
+          await firesService.addChat(editable.value);
+        } catch (error) {
+          console.error("[]", error);
+          Pop.error(error);
+        }
+      },
+    };
+  },
+};
 </script>
 
-<style lang="scss" scoped>
-</style>
+<style lang="scss" scoped></style>
