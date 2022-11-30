fe800c539ae4e0cb6cfb5fd039cc5f969927d86b
Jacob Neitzell <107662321+JacobNeitzell@users.noreply.github.com>
2022-11-30 11:49:25 -0700

    about page photo adjust

--- Rcmf.client/src/pages/AboutPage.vue
+++ Rcmf.client/src/pages/AboutPage.vue
@@ -4,7 +4,7 @@
       <div class="col-md-12 d-flex justify-content-center">
         <img
           src="https://images-ext-2.discordapp.net/external/VUMO-JdjQhpPTHQGzprkkSXOMheJUjAd0HYChhUwTLM/https/s3.amazonaws.com/gs-waymarking-images/03a76656-afe3-44cf-8894-9aaf0a305b2f_l.jpg?width=693&height=468"
-          alt="Firefighter Statue" class="rounded">
+          alt="Firefighter Statue" class="rounded m-4 elevation-5">
       </div>
     </div>
 
@@ -12,17 +12,19 @@
     <div class="row">
       <div class="col-md-12 d-flex align-items-center">
         <nav>
-          <div class="nav nav-tabs" id="nav-tab" role="tablist">
+          <div class="nav nav-tabs m-3 p-2" id="nav-tab" role="tablist">
             <button class="nav-link active" id="nav-home-tab" data-bs-toggle="tab" data-bs-target="#nav-home"
               type="button" role="tab" aria-controls="nav-home" aria-selected="true">About Foundation <img
                 src="http://ourfallenbrother.org/pictures/memorial.jpg" alt="Coos Bay FireFighters Memorial"
-                class="coosbay"></button>
-            <button class="nav-link" id="nav-profile-tab" data-bs-toggle="tab" data-bs-target="#nav-profile"
+                class="coosbay elevation-5 rounded"></button>
+            <button class="nav-link m-3 p-2" id="nav-profile-tab" data-bs-toggle="tab" data-bs-target="#nav-profile"
               type="button" role="tab" aria-controls="nav-profile" aria-selected="false">About Randy <img
-                src="http://ourfallenbrother.org/pictures/randy.jpg" alt="Randy E Carepenter"></button>
-            <button class="nav-link" id="nav-contact-tab" data-bs-toggle="tab" data-bs-target="#nav-contact"
+                src="http://ourfallenbrother.org/pictures/randy.jpg" alt="Randy E Carepenter"
+                class="elevation-5 rounded"></button>
+            <button class="nav-link m-3 p-2" id="nav-contact-tab" data-bs-toggle="tab" data-bs-target="#nav-contact"
               type="button" role="tab" aria-controls="nav-contact" aria-selected="false">Apply For A Grant <img
-                src="http://ourfallenbrother.org/pictures/equipment.jpg" alt="Fireman with a hammer"></button>
+                src="https://images.unsplash.com/photo-1632844281300-7ce850388164?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+                alt="plane putting out fire" class="elevation-5 rounded twofiremen"></button>
           </div>
         </nav>
         <div class="tab-content" id="nav-tabContent">
@@ -53,5 +55,10 @@ export default {
 .coosbay {
   height: 15rem;
   width: 15rem;
+}
+
+.twofiremen {
+  height: 16rem;
+  width: 16.5rem;
 }
 </style>
