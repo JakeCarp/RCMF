ab41eba6c379840448fef25dd453d73fb6fc7bca
Jacob Neitzell <107662321+JacobNeitzell@users.noreply.github.com>
2022-12-01 13:08:33 -0700

    Adjusting and cleaning up about page

--- Rcmf.client/src/pages/AboutPage.vue
+++ Rcmf.client/src/pages/AboutPage.vue
@@ -14,13 +14,14 @@
         <nav>
           <div class="nav nav-tabs justify-content-around" id="nav-tab" role="tablist">
             <button class="nav-link active bg-dark navButton" id="nav-home-tab" data-bs-toggle="tab"
-              data-bs-target="#nav-home" type="button" role="tab" aria-controls="nav-home" aria-selected="true">What Am
+              data-bs-target="#nav-support" type="button" role="tab" aria-controls="nav-support"
+              aria-selected="true">What Am
               I Supporting?</button>
             <button class="nav-link bg-dark navButton" id="nav-profile-tab" data-bs-toggle="tab"
               data-bs-target="#nav-profile" type="button" role="tab" aria-controls="nav-profile"
               aria-selected="false">Who Is Randy Carpenter?</button>
-            <button class="nav-link bg-dark navButton" id="nav-profile-tab" data-bs-toggle="tab"
-              data-bs-target="#nav-profile" type="button" role="tab" aria-controls="nav-profile"
+            <button class="nav-link bg-dark navButton" id="nav-events-tab" data-bs-toggle="tab"
+              data-bs-target="#nav-events" type="button" role="tab" aria-controls="nav-events"
               aria-selected="false">What Events Are Held?</button>
             <button class="nav-link bg-dark navButton" id="nav-contact-tab" data-bs-toggle="tab"
               data-bs-target="#nav-contact" type="button" role="tab" aria-controls="nav-contact"
@@ -36,10 +37,100 @@
 
             <!-- !tab1 -->
           </div>
-          <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab" tabindex="0">
+          <div class="tab-pane fade" id="nav-support" role="tabpanel" aria-labelledby="nav-support-tab" tabindex="0">
+            <div>
+              <h2>What Am I Supporting?</h2>
+            </div>
+            <div>
+            </div>
+          </div>
 
-            About Randy <img src="http://ourfallenbrother.org/pictures/randy.jpg" alt="Randy E Carepenter"
-              class="elevation-5 rounded">
+
+          <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab" tabindex="0">
+            <div class="row">
+              <div class="col-md-8 col-lg-12 d-flex justify-content-center">
+                <h2>About Randy</h2>
+              </div>
+              <div class="col-md-6 col-lg-12 d-flex justify-content-center ">
+                <img src="http://ourfallenbrother.org/pictures/randy.jpg" alt="Randy E Carepenter"
+                  class="elevation-5 rounded">
+              </div>
+              <div class="col-md-6 col-lg-12">
+                <p>Coos Bay Fire Department
+                  A career firefighter, Captain Randy E. Carpenter, 46, died November 25, 2002, when the burning roof of
+                  a
+                  business in
+                  downtown Coos Bay collapsed. Randy had been a firefighter for a total of 23 years. He began his career
+                  as
+                  a volunteer,
+                  and later became a full-time firefighter with Baker City Fire Department. In February 1989, he
+                  accepted
+                  a
+                  position with
+                  Coos Bay Fire Department and his dedication there truly established his legacy. He was Baker City's
+                  1983
+                  'Firefighter of
+                  the Year', and Coos Bay's 1991 'Firefighter of the Year'. He was also Coos Bay"s 1999 'EMT of the
+                  Year'.
+                  Randy's interest in firefighting began early in his life when he would accompany his father, a
+                  volunteer
+                  firefighter for
+                  Baker City for 46 years, to the firestation. During the time he was a volunteer, Randy was very active
+                  in
+                  the Oregon
+                  Volunteer Firefighter Association and served in all State Association offices, including president.
+                  Randy
+                  was a
+                  consummate professional and was highly qualified in all operational and procedural aspects of his job.
+                  He
+                  was part of
+                  the original efforts to bring a hazardous materials unit to Coos Bay and was a specialist in HAZMAT.
+                  He
+                  was qualified
+                  and certified to teach all subjects of firefighting and regularly taught courses in CPR and EMT Basic
+                  at
+                  the local
+                  college in Coos Bay. He took great pride in his job and treasured the trust and respect of all his
+                  firefighter brethren.
+                  He was an active member of the International Association of Firefighters, IAFF, Local 2935 (serving as
+                  President in
+                  1991), and the Regional Hazardous Materials Team. He was also skilled in construction and built a
+                  successful contracting
+                  business with his good friend Jim Bush.
+                  The love of family was very foremost in Randy's life. He was so proud of and totally dedicated to his
+                  two
+                  girls, Sarah
+                  and Stephanie, with priority in providing for their needs. His parents, Wayne and Dru, three brothers,
+                  Kerry, Brad, and
+                  Jeff, their spouses and families also survived him.
+                  Randy was usually very successful when he took his boat out "crabbing", and was always happy when he
+                  could
+                  take time to
+                  go water-skiing, at which he was very adept. He especially loved spending time in Eastern Oregon (Dad
+                  &
+                  Mom referred to
+                  him as their "homing pigeon") where he would hunt deer, elk, and antelope, along with working with
+                  longtime friends on
+                  the Bunch Ranch near Durkee. While Randy had a wonderful sense of humor and enjoyed playing a
+                  practical
+                  joke, he never
+                  wanted the limelight, just a way to be of help. His honesty and integrity were unquestionable. All his
+                  family and his
+                  countless friends, who were like family to him, will miss Randy beyond description.
+                  The Randy Carpenter Memorial Firefighter Training Fund has been established to provide funds to aid in
+                  needed training
+                  for Oregon and Southwest Idaho firefighters and emergency personnel.</p>
+              </div>
+            </div>
+          </div>
+          <div class="tab-pane fade" id="nav-events" role="tabpanel" aria-labelledby="nav-events-tab" tabindex="0">
+            <div>
+              <h2>What Events Are Held?</h2>
+            </div>
+            <div> <img
+                src="https://images.unsplash.com/photo-1632844281300-7ce850388164?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+                alt="plane putting out fire" class="elevation-5 rounded twofiremen">
+            </div>
           </div>
           <div class="tab-pane fade" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab" tabindex="0">
             Apply For A Grant <img
